b0a819a022268bb1132d481d064f5eee235945c4
ajinkya887 <ajinkyasontakke07@gmail.com>
2025-05-09 15:28:22 +0530

    Integrated mongodb database and performed crud oprations

--- src/main/java/com/example/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/example/journalApp/controller/JournalEntryController.java
@@ -1,42 +1,54 @@
 package com.example.journalApp.controller;
 
 import com.example.journalApp.entity.JournalEntry;
+import com.example.journalApp.repository.JournalEntryRepository;
+import com.example.journalApp.service.JournalEntryService;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.print.attribute.standard.JobHoldUntil;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Locale;
-import java.util.Map;
+import java.time.LocalDateTime;
+import java.util.*;
 
 @RestController
 @RequestMapping("/journal")
 public class JournalEntryController {
-    private Map<Long, JournalEntry> journalEntries = new HashMap<>();
+
+    @Autowired
+    private JournalEntryService journalEntryService;
 
     @GetMapping
-    public ArrayList<JournalEntry> getAll(){
-        return new ArrayList<>(journalEntries.values());
+    public List<JournalEntry> getAll(){
+        return journalEntryService.getAllEntries();
     }
 
     @PostMapping
     public String createEntry(@RequestBody JournalEntry myEntry){
-        journalEntries.put(myEntry.getId(), myEntry);
+        myEntry.setDate(LocalDateTime.now());
+        journalEntryService.saveEntry(myEntry);
         return "Journal Entry Added Successfully";
     }
 
     @GetMapping("/{myID}")
-    public JournalEntry getJournalEntryById(@PathVariable Long myID){
-        return journalEntries.get(myID);
+    public JournalEntry getJournalEntryById(@PathVariable ObjectId myID){
+        return journalEntryService.findById(myID).orElse(null);
     }
 
     @DeleteMapping("/{myID}")
-    public JournalEntry deleteJournalEntryById(@PathVariable Long myID){
-        return journalEntries.remove(myID);
+    public String deleteJournalEntryById(@PathVariable ObjectId myID){
+        journalEntryService.deleteById(myID);
+        return "Deleted Sucessfully";
     }
 
     @PutMapping("/{myID}")
-    public JournalEntry updateJournalById(@PathVariable Long myID, @RequestBody JournalEntry myEntry){
-        return journalEntries.put(myID, myEntry);
+    public JournalEntry updateJournalById(@PathVariable ObjectId myID, @RequestBody JournalEntry newEntry){
+        JournalEntry old = journalEntryService.findById(myID).orElse(null);
+        if(old != null){
+            old.setTitle(newEntry.getTitle() != null && !newEntry.getTitle().equals("") ? newEntry.getTitle() : old.getTitle());
+            old.setContent(newEntry.getContent() != null && !newEntry.equals("") ? newEntry.getContent() : old.getContent());
+        }
+        journalEntryService.saveEntry(old);
+        return old;
     }
 }
--- src/main/java/com/example/journalApp/entity/JournalEntry.java
+++ src/main/java/com/example/journalApp/entity/JournalEntry.java
@@ -1,9 +1,35 @@
 package com.example.journalApp.entity;
 
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")
 public class JournalEntry {
-    private long id;
+    @Id
+    private ObjectId id;
     private String title;
     private String content;
+    private LocalDateTime date;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
 
     public String getContent() {
         return content;
@@ -21,11 +47,4 @@ public class JournalEntry {
         this.title = title;
     }
 
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
 }
