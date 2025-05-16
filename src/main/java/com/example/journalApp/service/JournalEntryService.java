21864d2727ae433d1ca2d68958fc6667cf3ae53c
ajinkya887 <ajinkyasontakke07@gmail.com>
2025-05-16 10:11:42 +0530

    added transactional annotation and mongodb atlas

--- src/main/java/com/example/journalApp/service/JournalEntryService.java
+++ src/main/java/com/example/journalApp/service/JournalEntryService.java
@@ -6,6 +6,7 @@ import com.example.journalApp.repository.JournalEntryRepository;
 import org.bson.types.ObjectId;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -19,12 +20,20 @@ public class JournalEntryService {
     @Autowired
     private UserService userService;
 
+    @Transactional
     public void saveEntry(JournalEntry journalEntry, String userName){
-        User user = userService.findByUserName(userName);
-        journalEntry.setDate(LocalDateTime.now());
-        JournalEntry saved = journalEntryRepository.save(journalEntry);
-        user.getJournalEntries().add(saved);
-        userService.saveEntry(user);
+        try {
+            User user = userService.findByUserName(userName);
+            journalEntry.setDate(LocalDateTime.now());
+            JournalEntry saved = journalEntryRepository.save(journalEntry);
+            user.getJournalEntries().add(saved);
+//            just for the testing the trasactional annotation
+//            user.setUserName(null);
+            userService.saveEntry(user);
+        } catch (Exception e) {
+            throw new RuntimeException("An error occured while saving the entry: "+ e);
+        }
+
     }
 
     public void saveEntry(JournalEntry journalEntry){
